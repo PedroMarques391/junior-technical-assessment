128b6727ae6dbd206f8570db7280cbd57b56af0a
Pedro Marques <pedromarques391@gmail.com>
2026-01-22 09:40:58 -0300

    feat: create method put to update estoques.

---
+++ app/api/estoque/[id]/route.ts
@@ -0,0 +1,28 @@
+import * as service from "@/services/estoque.service";
+import { NextResponse } from "next/server";
+
+interface Params {
+  params: Promise<{ id: string }>;
+}
+
+export async function PUT(request: Request, { params }: Params) {
+  try {
+    const id = BigInt((await params).id);
+    const body = await request.json();
+    const updatedEstoque = await service.updateEstoque(id, body);
+    const updatedEstoqueSerialized = JSON.parse(
+      JSON.stringify(updatedEstoque, (key, value) =>
+        typeof value === "bigint" ? value.toString() : value,
+      ),
+    );
+    return NextResponse.json(updatedEstoqueSerialized, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        erro:
+          error instanceof Error ? error.message : "Falha ao atualizar estoque",
+      },
+      { status: 500 },
+    );
+  }
+}
