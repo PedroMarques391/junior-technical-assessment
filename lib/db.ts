78e66f2f57d51633f85cc3934d0d19a5f82ece3f
Pedro Marques <pedromarques391@gmail.com>
2026-01-22 09:39:24 -0300

    feat: create models: "estoque" and "estoque_movimentacoes".

--- lib/db.ts
+++ lib/db.ts
@@ -3,8 +3,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 import { Pool } from "pg";
 
-console.log("Database URL:", process.env.DATABASE_URL);
-
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
